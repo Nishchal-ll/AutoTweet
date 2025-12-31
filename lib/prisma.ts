75ccbc3770aa2a2fdf673b1d3e092d0e2f4e79eb
Nishchal-ll <acharyanischal2004@gmail.com>
2025-12-31 19:33:08 +0545

    Fix Prisma Vercel build

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -13,15 +13,12 @@
 // export default prisma;
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
 };
 
 export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["error"],
-  });
+  globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
